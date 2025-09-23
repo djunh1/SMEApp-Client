2fe8b79b60e32593a68a521b1edd642df3062b34
djunh1 <djunh1@gmail.com>
2025-09-23 16:36:08 -0400

    Add endpoint and cerdential type for login

---
+++ src/api/admin/users.ts
@@ -0,0 +1,20 @@
+import { ILoginCredentials } from "@/models/iLoginCredentials";
+import axios from "axios";
+
+export const authenticate = (creds: ILoginCredentials) => {
+    axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+    const baseUrl = process.env.VUE_APP_BASE_URL
+
+    return axios.post(baseUrl + 'api/v1/token', {
+        username: creds.username,
+        password: creds.password
+    }, {}).then((response => {
+        return response.status === 200
+            ? Promise.resolve(response)
+            : Promise.reject()
+    })).catch((error) => {
+        if (error.messages === "Network error") return error.message;
+        return error.response;
+    });
+}
